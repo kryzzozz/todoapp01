3529eabc0eb71f1c86807c08765fb5fddcc32b08
Christian Osuna <chris.osuna@gmail.com>
2024-02-14 20:03:21 -0700

    first branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoapp';
+  readonly APIURL = 'http://localhost:5038/api/todoapp/';
+  
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  public notes : any = [];
+  
+  refreshNotes() {
+    this.http.get(this.APIURL+'GetNotes').subscribe(data => {
+      this.notes = data;
+    })
+  }
+
+  ngOnInit() {
+    this.refreshNotes();
+  }
+
+  addNotes() {
+    var newNotes = (<HTMLInputElement>document.getElementById('newNotes')).value;
+    var formData = new FormData();
+    formData.append('newNotes', newNotes);
+    this.http.post(this.APIURL+'AddNotes', formData).subscribe(data => {
+      alert(data);
+      this.refreshNotes();
+    })
+  }
+
+  deleteNotes(id: any) {
+    this.http.delete(this.APIURL + 'DeleteNotes?id=' + id).subscribe(data => {
+      alert(data);
+      this.refreshNotes();
+    })
+  }
 }
+
